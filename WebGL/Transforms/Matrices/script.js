71a9bda6a791bc46ce4585ab25a3a12e3f95a7ed
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-11-03 15:58:44 -0300

    added 2D matrices example

---
+++ WebGL/Transforms/Matrices/script.js
@@ -0,0 +1,153 @@
+const vertexShaderSource = `#version 300 es
+    in vec2 a_position;
+    uniform vec2 u_resolution;
+    uniform vec2 u_translation;
+    uniform vec2 u_rotation;
+    uniform vec2 u_scale;
+
+    void main() {
+      vec2 scaledPosition = a_position * u_scale;
+      vec2 rotatedPosition = vec2(scaledPosition.x * u_rotation.y + scaledPosition.y * u_rotation.x, scaledPosition.y * u_rotation.y - scaledPosition.x * u_rotation.x);
+      vec2 position = rotatedPosition + u_translation;
+      vec2 zeroToOne = position / u_resolution;
+      vec2 zeroToTwo = zeroToOne * 2.0;
+      vec2 clipSpace = zeroToTwo - 1.0;
+      gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+    }
+  `;
+
+const fragmentShaderSource = `#version 300 es
+    precision highp float;
+    uniform vec4 u_color;
+    out vec4 outColor;
+
+    void main() {
+      outColor = u_color;
+    }
+  `;
+
+function main() {
+  const canvas = document.querySelector("#canvas");
+  /** @type {WebGL2RenderingContext} */
+  const gl = canvas.getContext("webgl2");
+  if (!gl) {
+    console.log("WebGL not supported!")
+  }
+
+  const program = webglUtils.createProgramFromSources(gl, [vertexShaderSource, fragmentShaderSource]);
+
+  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+  const colorLocation = gl.getUniformLocation(program, "u_color");
+  const translationLocation = gl.getUniformLocation(program, "u_translation");
+  const rotationLocation = gl.getUniformLocation(program, "u_rotation");
+  const scaleLocation = gl.getUniformLocation(program, "u_scale");
+
+  const positionBuffer = gl.createBuffer();
+  const vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  setGeometry(gl);
+
+  const size = 2;
+  const type = gl.FLOAT;
+  const normalize = false;
+  const stride = 0;
+  const offset = 0;
+
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+
+  // VARIÁVEIS DE TRANSLAÇÃO //
+  const translation = [150, 150];
+  const rotation = [0, 1];
+  const scale = [1, 1];
+  const color = [Math.random(), Math.random(), Math.random(), 1];
+
+  drawScene();
+
+  webglLessonsUI.setupSlider("#x", { slide: updatePosition(0), max: gl.canvas.width });
+  webglLessonsUI.setupSlider("#y", { slide: updatePosition(1), max: gl.canvas.height });
+  webglLessonsUI.setupSlider("#angle", { slide: updateAngle, max: 360 });
+  webglLessonsUI.setupSlider("#scaleX", { value: scale[0], slide: updateScale(0), min: -5, max: 5, step: 0.01, precision: 2 });
+  webglLessonsUI.setupSlider("#scaleY", { value: scale[1], slide: updateScale(1), min: -5, max: 5, step: 0.01, precision: 2 });
+
+  function updatePosition(index) {
+    return function (event, ui) {
+      translation[index] = ui.value;
+      drawScene();
+    }
+  }
+
+  function updateAngle(event, ui) {
+    var angleInDegrees = 360 - ui.value;
+    var angleInRadians = angleInDegrees * Math.PI / 180;
+    rotation[0] = Math.sin(angleInRadians);
+    rotation[1] = Math.cos(angleInRadians);
+    drawScene();
+  }
+
+  function updateScale(index) {
+    return function (event, ui) {
+      scale[index] = ui.value;
+      drawScene();
+    }
+  }
+
+  function drawScene() {
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.useProgram(program);
+
+    gl.bindVertexArray(vao);
+
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+    gl.uniform4fv(colorLocation, color);
+
+    gl.uniform2fv(translationLocation, translation);
+
+    gl.uniform2fv(rotationLocation, rotation);
+
+    gl.uniform2fv(scaleLocation, scale);
+
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 18;
+    gl.drawArrays(primitiveType, offset, count);
+  }
+}
+
+function setGeometry(gl) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      0, 0,
+      30, 0,
+      0, 150,
+      0, 150,
+      30, 0,
+      30, 150,
+      30, 0,
+      100, 0,
+      30, 30,
+      30, 30,
+      100, 0,
+      100, 30,
+      30, 60,
+      67, 60,
+      30, 90,
+      30, 90,
+      67, 60,
+      67, 90,
+    ]),
+    gl.STATIC_DRAW);
+}
+
+main();
